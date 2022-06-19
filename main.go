7ab97decbbe42b2054f8f629d81e76f10dad97e3
Michael Bozhilov <michaelbozhilov@gmail.com>
2022-06-19 15:41:31 +0300

    Add sample article slice

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"encoding/json"
 	"log"
 	"net/http"
 
@@ -10,7 +11,7 @@ import (
 
 // Event Model
 type Event struct {
-	ID        int    `json:id`
+	ID        string `json:"id"`
 	Title     string `json:"title"`
 	Place     string `json:"place"`
 	StartTime string `json:"startTime"`
@@ -20,10 +21,10 @@ type Event struct {
 
 // Article Model
 type Article struct {
-	ID         int      `json:id`
+	ID         string   `json:"id"`
 	Title      string   `json:"title"`
 	Author     *Author  `json: "author"`
-	Paragraphs []string `json: paragraphs`
+	Paragraphs []string `json: "paragraphs"`
 }
 
 // Author
@@ -32,6 +33,9 @@ type Author struct {
 	Lastname  string `json:"lastName"`
 	LinkedIn  string `json:"linkedin"`
 }
+
+// Slice of Articles
+var articles []Article
 
 // Get Events
 func getEvents(w http.ResponseWriter, r *http.Request) {
@@ -47,7 +51,8 @@ func createEvent(w http.ResponseWriter, r *http.Request) {
 }
 
 func getArticles(w http.ResponseWriter, r *http.Request) {
-
+	w.Header().Set("Content-Type", "application/json")
+	json.NewEncoder(w).Encode(articles)
 }
 
 func getArticle(w http.ResponseWriter, r *http.Request) {
@@ -59,6 +64,11 @@ func createArticle(w http.ResponseWriter, r *http.Request) {
 }
 
 func main() {
+	// Mock data - implement DB
+	articles = append(articles, Article{ID: "1", Title: "Welcome to The Hub", Author: &Author{Firstname: "Michael", Lastname: "Bozhilov"}, Paragraphs: []string{
+		"sdfhsdjkfsdjkhfh", "sdjafhjsdfhkjsdfhj", "sdjkfhsdjfhjks",
+	}})
+
 	// Init Router
 	router := mux.NewRouter()
 
@@ -66,12 +76,12 @@ func main() {
 	// Events Endpoints
 	router.HandleFunc("/api/events", getEvents).Methods("GET")     // get list of events
 	router.HandleFunc("/api/events/{id}", getEvent).Methods("GET") // get specific event
-	router.HandleFunc("/api/events/", createEvent).Methods("PUT")  // create a event
+	router.HandleFunc("/api/events/", createEvent).Methods("POST") // create a event
 
 	// Articles Endpoints
 	router.HandleFunc("/api/articles", getArticles).Methods("GET")     // get list of articles
 	router.HandleFunc("/api/articles/{id}", getArticle).Methods("GET") // get specific article
-	router.HandleFunc("/api/articles/", createArticle).Methods("PUT")  // create an article
+	router.HandleFunc("/api/articles/", createArticle).Methods("POST") // create an article
 
 	// Serve and log exceptions
 	log.Fatal(http.ListenAndServe(":8000", router))
